79dd484e71382dfde4443fea1677f6cd6af36b86
VIPIN KUMAR <vipin.kumar@epam.com>
2023-10-09 23:10:06 +0530

    Task 3 changes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
