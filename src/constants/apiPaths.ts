7a6f3dc13202da4b474ec505e252851349f86e3d
kumar-vipin <kumar.vipin1189@gmail.com>
2023-11-08 23:01:51 +0530

    bff and import api link changes (#4)
    Co-authored-by: VIPIN KUMAR <vipin.kumar@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://ze1nnxjej4.execute-api.us-east-1.amazonaws.com/dev",
-  bff: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://emq14e1qhj.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://emq14e1qhj.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://y6jyam9j9f.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://kucqhiw2r2.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://emq14e1qhj.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
