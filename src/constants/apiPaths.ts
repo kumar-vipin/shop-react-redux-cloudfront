d9b531745f4a8d30b69e5c13a18f99d6f00b4155
kumar-vipin <kumar.vipin1189@gmail.com>
2023-11-08 22:56:34 +0530

    task-5 (#3)
    Co-authored-by: VIPIN KUMAR <vipin.kumar@epam.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://dth4pg1nck1.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://ze1nnxjej4.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://dth4pg1nck.execute-api.us-east-1.amazonaws.com/dev",
 };
